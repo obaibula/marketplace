06bd6c0406de635a5514d6df00ff1fad491f7a0a
obaibula <baibula.oo@gmail.com>
2023-07-15 19:39:40 +0300

    fetch city by ip4

--- src/main/java/com/onrender/navkolodozvillya/location/LocationController.java
+++ src/main/java/com/onrender/navkolodozvillya/location/LocationController.java
@@ -15,7 +15,26 @@ public class LocationController {
 
     @GetMapping
     public GeoIP getCity(HttpServletRequest request){
-        var ipAddress = request.getRemoteAddr();
+
+
+        var ipAddress = request.getHeader("X-FORWARDED-FOR");
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
         return geoIPLocationService.getIpLocation(ipAddress, request);
     }
 }
