e4ae91884295224bf2343f7ac97cc2c01920c2c8
obaibula <baibula.oo@gmail.com>
2023-07-23 08:52:49 +0300

    minor improvements

--- src/main/java/com/onrender/navkolodozvillya/auth/AuthenticationResponse.java
+++ src/main/java/com/onrender/navkolodozvillya/auth/AuthenticationResponse.java
@@ -1,7 +1,5 @@
 package com.onrender.navkolodozvillya.auth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record AuthenticationResponse(
         String accessToken,
         String refreshToken) {
